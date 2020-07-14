e47244d654953ae2249e4f706fe9e76fa362a6cf
gin-yamato <20j70028sg@edu.tca.ac.jp>
2020-07-14 18:35:14 +0900

    tekitou2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿//BMI計算プログラム
+
+using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float height = GetInputAndparse("身長を入力してください。(m)");
+            float weight = GetInputAndparse("体重を入力してください(kg)");
+            float bmi = GetBMI(height, weight);
+
+            Console.WriteLine("BMIは" + bmi + " です");
+        }
+
+        static float GetInputAndparse(string message)
+        {
+            float parsed = 0f;
+            bool parseSuccess = false;
+            while (!parseSuccess)
+            {
+                Console.WriteLine(message);
+                string input = Console.ReadLine();
+                parseSuccess = float.TryParse(input, out parsed);
+                if (!parseSuccess)
+                    Console.WriteLine("数値を入力してください");
+            }
+            return parsed;
+        }
+        static float GetBMI(float height, float weight)
+        {
+            return weight / (height * height);
+        }
+    }
+}
